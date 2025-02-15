01ce795d1a1adbf601a0eb3df912fd990d235bcf
ZihanWang314 <510642032wzh@gmail.com>
2025-02-15 07:50:28 +0000

    update kl losstype support

--- ragen/train.py
+++ ragen/train.py
@@ -106,6 +106,7 @@ def get_rl_train_command(config: Dict[str, Any]) -> str:
         # f"critic.ppo_micro_batch_size={config['training']['micro_batch_size']}",
         # f"algorithm.kl_ctrl.kl_coef={config['optimization']['kl_coef']}", # This line is not effective and does not change any training dynamics for now, since "gae" is not implemented in this repo. This will be more useful when use_kl_loss=False is allowed. Specifically, GAE(PPO) needs a trained critic with "kl". Note that we are currently not having a really trained critic so we cannot make this to True.
         f"actor_rollout_ref.actor.kl_loss_coef={config['optimization']['kl_coef']}", # for use_kl_loss=True. ARPO/BRPO/GRPO needs the original model with "low_var_kl"
+        f"actor_rollout_ref.actor.kl_loss_type={config['optimization']['kl_loss_type']}",
         f"algorithm.no_think_rl={config['training']['no_think_rl']}",
         f"actor_rollout_ref.rollout.n_agent={config['training']['n_rollout']}",
         f"actor_rollout_ref.rollout.temperature={config['training']['temperature']}",
