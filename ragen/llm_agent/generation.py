e07f30075bc2240ea3a0a267c40bc114cd98ec5f
williamzhangNU <pingyuezhang2029@u.northwestern.edu>
2025-02-20 23:24:47 -0600

    remove postprocess of generated string

--- ragen/llm_agent/generation.py
+++ ragen/llm_agent/generation.py
@@ -68,9 +68,9 @@ class LLMGenerationManager:
             skip_special_tokens=True
         )
 
-        responses_str = [resp.split('</answer>')[0] + '</answer>' 
-                    if '</answer>' in resp else resp 
-                    for resp in responses_str]
+        # responses_str = [resp.split('</answer>')[0] + '</answer>' 
+        #             if '</answer>' in resp else resp 
+        #             for resp in responses_str]
         
         if self.config.state_masking:
             # Escape special characters in markers for regex
@@ -238,11 +238,10 @@ class LLMGenerationManager:
                 k: v[active_mask] for k, v in rollings.batch.items()
             })
             gen_output = self._generate_with_gpu_padding(rollings_active)
-            # print('rollings_active response:', rollings_active.batch['input_ids'][0], 'shape:', rollings_active.batch['input_ids'].shape)
-            # print('gen_output response:', gen_output.batch['responses'][0], 'shape:', gen_output.batch['responses'].shape)
-            # print('gen_output response str:', self.tokenizer.decode(gen_output.batch['responses'][0]))
-            # print('gen_output attention mask:', gen_output.batch['attention_mask'][0], 'shape:', gen_output.batch['attention_mask'].shape)
-            # print("eos token id:", self.tokenizer.eos_token_id)
+            # print('gen_output response str 1:', self.tokenizer.decode(gen_output.batch['responses'][0]))
+            # print('gen_output response str 2:', self.tokenizer.decode(gen_output.batch['responses'][1]))
+            # print('gen_output response str 3:', self.tokenizer.decode(gen_output.batch['responses'][2]))
+            # print('gen_output response str 4:', self.tokenizer.decode(gen_output.batch['responses'][3]))
 
             meta_info = gen_output.meta_info            
             responses_ids, responses_str = self._postprocess_responses(gen_output.batch['responses'],envs=envs)
