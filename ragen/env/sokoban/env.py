add rollout filter; modify config; debug sokoban; change loss mask; add response_length metric; remove max_mean filtering; correct kl_loss calculation; add job running script

ragen/env/sokoban/env.py
@@ -54,7 +54,7 @@ class SokobanEnv(BaseDiscreteActionEnv, GymSokobanEnv):
         render_mode = mode if mode is not None else self.render_mode
         if render_mode == 'text':
             room = np.where((self.room_state == 5) & (self.room_fixed == 2), 6, self.room_state)
-            return '\n'.join(''.join(self.GRID_LOOKUP.get(cell, "?") for cell in row) for row in room)
+            return '\n'.join(''.join(self.GRID_LOOKUP.get(cell, "?") for cell in row) for row in room.tolist())
         elif render_mode == 'rgb_array':
             return self.get_image(mode='rgb_array', scale=1)
         else:

ragen/llm_agent/ctx_manager.py
@@ -19,7 +19,7 @@ from tensordict import TensorDict
 from dataclasses import asdict
 register_resolvers()
 
-def get_loss_mask_and_scores(input_ids: torch.Tensor, tokenizer: AutoTokenizer, all_scores: List[List[float]] = None, use_turn_scores: bool = False):
+def get_masks_and_scores(input_ids: torch.Tensor, tokenizer: AutoTokenizer, all_scores: List[List[float]] = None, use_turn_scores: bool = False):
     """
     input_ids: shape (bsz, seq_len)
     Get loss mask that only learns between <|im_start|>assistant and <|im_end|>. Currently only supports qwen.
@@ -28,7 +28,8 @@ def get_loss_mask_and_scores(input_ids: torch.Tensor, tokenizer: AutoTokenizer, 
     special_token = tokenizer.encode("<|im_start|>")[0]
     turn_starts = torch.where(input_ids == special_token, 1, 0)
     turn_indicators = torch.cumsum(turn_starts, dim=-1)
-    loss_mask = (turn_indicators % 2 == 1) & (turn_indicators > 1) # only learns all assistant turns
+    response_mask = (turn_indicators % 2 == 1) & (turn_indicators > 1) # only learns all assistant turns
+    loss_mask = (turn_indicators > 1) # learns everything after system prompt
 
     reward_token = tokenizer.encode("<|im_end|>")[0]
     score_tensor = torch.zeros_like(input_ids, dtype=torch.float32)
@@ -44,7 +45,7 @@ def get_loss_mask_and_scores(input_ids: torch.Tensor, tokenizer: AutoTokenizer, 
     loss_mask = loss_mask[:, :-1] # remove the last token
     score_tensor = score_tensor[:, 1:] # remove the first token
 
-    return loss_mask, score_tensor
+    return loss_mask, score_tensor, response_mask
 
 
 
@@ -245,8 +246,9 @@ class ContextManager:
         position_ids = attention_mask.cumsum(dim=-1)
         if prepare_for_update:
             scores = [[i['reward'] for i in env_output['history']] for env_output in env_outputs]
-            loss_mask, score_tensor = get_loss_mask_and_scores(input_ids, self.tokenizer, scores, use_turn_scores=self.config.agent_proxy.use_turn_scores)
+            loss_mask, score_tensor, response_mask = get_masks_and_scores(input_ids, self.tokenizer, scores, use_turn_scores=self.config.agent_proxy.use_turn_scores)
             normalized_score_tensor = self._normalize_score_tensor(score_tensor, env_outputs)
+            response_length = response_mask.sum(dim=-1).float().mean().item()
 
         llm_inputs = DataProto()
         llm_inputs.batch = TensorDict({
@@ -277,6 +279,7 @@ class ContextManager:
                 key: np.sum(value) / self.env_nums[key.split("/")[0]]
                 for key, value in metrics.items()
             }
+            metrics["response_length"] = response_length
             llm_inputs.meta_info = {"metrics": metrics}
         return llm_inputs
 

ragen/workers/actor/dp_actor.py
@@ -35,6 +35,20 @@ from flash_attn.bert_padding import pad_input, unpad_input, rearrange, index_fir
 
 __all__ = ['DataParallelPPOActor']
 
+def compute_policy_loss(old_log_prob, log_prob, advantages, eos_mask, cliprange):
+    """Adapted from verl/verl/trainer/ppo/core_algos.py
+    """
+    negative_approx_kl = log_prob - old_log_prob
+    ratio = torch.exp(negative_approx_kl)
+    ppo_kl = verl_F.masked_mean(-negative_approx_kl, eos_mask)
+
+    pg_losses = -advantages * ratio
+    pg_losses2 = -advantages * torch.clamp(ratio, 1.0 - cliprange[0], 1.0 + cliprange[1])
+
+    pg_loss = verl_F.masked_mean(torch.max(pg_losses, pg_losses2), eos_mask)
+    pg_clipfrac = verl_F.masked_mean(torch.gt(pg_losses2, pg_losses).float(), eos_mask)
+    return pg_loss, pg_clipfrac, ppo_kl
+
 
 class DataParallelPPOActor(BasePPOActor):
 
@@ -286,16 +300,16 @@ class DataParallelPPOActor(BasePPOActor):
                     old_log_prob = data['old_log_probs']
                     advantages = data['advantages']
 
-                    clip_ratio = self.config.clip_ratio
+                    clip_ratio = (self.config.clip_ratio_low, self.config.clip_ratio_high)
                     entropy_coeff = self.config.entropy_coeff
 
                     # all return: (bsz, response_length)
                     entropy, log_prob = self._forward_micro_batch(micro_batch=data, temperature=temperature)
-                    pg_loss, pg_clipfrac, ppo_kl = core_algos.compute_policy_loss(old_log_prob=old_log_prob,
-                                                                                  log_prob=log_prob,
-                                                                                  advantages=advantages,
-                                                                                  eos_mask=response_mask,
-                                                                                  cliprange=clip_ratio)
+                    pg_loss, pg_clipfrac, ppo_kl = compute_policy_loss(old_log_prob=old_log_prob,
+                                                                        log_prob=log_prob,
+                                                                        advantages=advantages,
+                                                                        eos_mask=response_mask,
+                                                                        cliprange=clip_ratio)
                     # compute entropy loss from entropy
                     entropy_loss = verl_F.masked_mean(entropy, response_mask)
 